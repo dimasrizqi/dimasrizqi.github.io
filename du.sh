8bb1389b336cd84f804ee4d39e1d5005be213782
dimasrizqi <dimasrizqi3128@yahoo.co.id>
2021-01-05 11:45:22 +0700

    Update du.sh

--- du.sh
+++ du.sh
@@ -5,3 +5,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu   $(lsb_release -cs) stable"
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
+rm -rf du.sh
